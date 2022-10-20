35ce799dfd9144b3bc306776be80d9dbe5c9fdd7
YassineIY <yassine.ibnyaich@gmail.com>
2022-10-20 02:43:49 +0100

    added html page

--- 09 - data structure/script.js
+++ 09 - data structure/script.js
@@ -7,3 +7,13 @@ const restaurant = {
   starterMenu: ["starter one", "starter two", "starter three"],
   mainMenu: ["main one", "main two", "main three"],
 };
+
+const array = [2, 3, 4];
+
+const a = array[0];
+const b = array[1];
+const c = array[2];
+
+const [x,y,z] = array;
+
+
